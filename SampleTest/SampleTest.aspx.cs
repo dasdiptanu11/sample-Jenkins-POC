f1f489fa912740c5eb354b398cc53f4bf99fdf25
Diptanu.Das <Diptanu.Das@effective.digital>
2018-09-26 19:44:30 +0530

    git commit to change folder structure #2

---
+++ SampleTest/SampleTest.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleTest
+{
+    public partial class SampleTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+
+            switch (OperatorDropDownList.SelectedValue)
+            {
+                case " ":
+                    result.Text = "Please select Operator";
+                    break;
+                case "+":
+                    int a = Convert.ToInt32(input1.Text);
+                    int b = Convert.ToInt32(input2.Text);
+                    result.Text = (a + b).ToString();
+                    break;
+                case "-":
+                    a = Convert.ToInt32(input1.Text);
+                    b = Convert.ToInt32(input2.Text);
+                    result.Text = (a - b).ToString();
+                    break;
+                case "*":
+                    a = Convert.ToInt32(input1.Text);
+                    b = Convert.ToInt32(input2.Text);
+                    result.Text = (a * b).ToString();
+                    break;
+                case "/":
+                    a = Convert.ToInt32(input1.Text);
+                    b = Convert.ToInt32(input2.Text);
+                    result.Text = ((a > b) ? a / b : b / a).ToString();
+                    break;
+                default:
+                    result.Text = "Please select Operator";
+                    break;
+            }
+        }
+    }
+}
